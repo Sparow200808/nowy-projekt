e3721f56b55dbfafb5448ff14e74439f7d1506e9
Sparow200808 <148242411+Sparow200808@users.noreply.github.com>
2024-01-09 15:38:29 +0000

    update

---
+++ backend/main.ts
@@ -0,0 +1,7 @@
+export function add(a: number, b:number): number{
+    return a + b;
+}
+
+Deno.serve((_request: Request) => {
+    return new Response("Hello, world");
+});
---
+++ frontend/src/App.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  async function getHome() {
+    const response = await fetch("http://locahost:8000",{
+      method: "GET"
+    })
+  
+    const data = await Response.text()
+    return data
+  }
+  
+  let display = "";
+  
+</script>
+
+<main>
+  
+</main>
